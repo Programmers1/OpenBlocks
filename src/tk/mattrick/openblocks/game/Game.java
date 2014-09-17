5e7ba6d210b7cd2e029b81be88b6c9f9f078ff76
mattrick16 <matthewhighrollers@gmail.com>
2014-09-16 17:55:59 -0700

    Camera test; break some stuff, fix some stuff, its all good :P

--- src/tk/mattrick/openblocks/game/Game.java
+++ src/tk/mattrick/openblocks/game/Game.java
@@ -6,16 +6,15 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
-import static org.lwjgl.opengl.GL11.*;
 import tk.mattrick.openblocks.blob.Blob;
 import tk.mattrick.openblocks.blob.BlobGenerator;
-import tk.mattrick.openblocks.blob.BlobRenderer;
-import tk.mattrick.openblocks.block.Block;
 import tk.mattrick.openblocks.block.BlockAir;
 import tk.mattrick.openblocks.block.BlockManager;
 import tk.mattrick.openblocks.block.BlockStone;
 import tk.mattrick.openblocks.location.World;
+import tk.mattrick.openblocks.player.Camera;
 import tk.mattrick.openblocks.render.Render;
 
 /**
@@ -25,10 +24,12 @@ import tk.mattrick.openblocks.render.Render;
 
 public class Game {
 
+	public boolean[] keys = new boolean[256];
+	Camera camera;
+
 	public Game() {
 
-		new BlockManager();
-		new BlobGenerator();
+		Camera camera = new Camera(0, 0, 0);
 
 		try {
 			Display.setDisplayMode(new DisplayMode(640,480));
@@ -38,22 +39,63 @@ public class Game {
 			e.printStackTrace();
 		}
 
-		Render.initGL();
+		Render.initGL(640, 480);
 
-		//Register Blocks
-		BlockManager.getInstance().addBlock(new BlockAir()).addBlock(new BlockStone());
+		float dx        = 0.0f;
+		float dy        = 0.0f;
+		float dt        = 0.0f;
+		long lastTime  = 0;
+		long time      = 0;
 
-		World w = new World("world");
-		Blob blob = new Blob(0, 0, 0, w);
+		float mouseSensitivity = 0.1f;
+		float movementSpeed = 3.0f;
+
+		Mouse.setGrabbed(true);
 
 		while (!Display.isCloseRequested()) {
 
-			//BlobRenderer.renderBlob(blob);
+			time = Sys.getTime();
+			dt = (time - lastTime)/1000.0f;
+			lastTime = time;
+
+			dx = Mouse.getDX();
+			dy = Mouse.getDY();
+
+			camera.yaw(dx * mouseSensitivity);
+
+			camera.pitch(-dy * mouseSensitivity);
+
+			if (Keyboard.isKeyDown(Keyboard.KEY_W))
+			{
+				camera.walkForward(movementSpeed*dt);
+				System.out.println("W" + movementSpeed*dt);
+			}
+			if (Keyboard.isKeyDown(Keyboard.KEY_S))
+			{
+				camera.walkBackwards(movementSpeed*dt);
+				System.out.println("S" + movementSpeed*dt);
+			}
+			if (Keyboard.isKeyDown(Keyboard.KEY_A))
+			{
+				camera.strafeLeft(movementSpeed*dt);
+				System.out.println("A" + movementSpeed*dt);
+			}
+			if (Keyboard.isKeyDown(Keyboard.KEY_D))
+			{
+				camera.strafeRight(movementSpeed*dt);
+				System.out.println("D" + movementSpeed*dt);
+			}
+
+			GL11.glLoadIdentity();
+
+			camera.lookThrough();
 
 			Render.renderTest();
 
 			Display.update();
-			Display.sync(60);
+
+
+
 		}
 
 	}
--- src/tk/mattrick/openblocks/render/Render.java
+++ src/tk/mattrick/openblocks/render/Render.java
@@ -1,11 +1,7 @@
 package tk.mattrick.openblocks.render;
 
-import static org.lwjgl.opengl.GL11.*;
-
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.GLU;
-import org.newdawn.slick.opengl.Texture;
-
-import tk.mattrick.openblocks.material.MaterialManager;
 
 
 /**
@@ -15,118 +11,104 @@ import tk.mattrick.openblocks.material.MaterialManager;
 
 public class Render {
 
-	static float xrot, yrot, zrot;
+	static float width, height;
 
-	public static void initGL() {
-		glEnable(GL_TEXTURE_2D);
-		glShadeModel(GL_SMOOTH);
-		glClearColor(0.439f, 0.69f, 1f, 0f);
-		glClearDepth(1.0);
-		glEnable(GL_DEPTH_TEST);
-		glDepthFunc(GL_LEQUAL);
+	public static void initGL(float w, float h) {
 
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
+		width = w;
+		height = h;
 
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
 
-		GLU.gluPerspective(
-				45.0f,
-				(float)640/(float)480,
-				0.1f,
-				100.0f);
+		//        Calculate the aspect ratio of the window
+		GLU.gluPerspective(45.0f,(width)/(height),0.1f,100.0f);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glLoadIdentity();
 
-		glMatrixMode(GL_MODELVIEW);
-		glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);                                    // Enable Texture Mapping ( NEW )
+		GL11.glShadeModel(GL11.GL_SMOOTH);
+		GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		GL11.glClearDepth(1.0f);
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glDepthFunc(GL11.GL_LEQUAL);
+		GL11.glHint(GL11.GL_PERSPECTIVE_CORRECTION_HINT, GL11.GL_NICEST);
 	}
 
 	public static void renderTest() {
-
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glLoadIdentity();
-		glTranslatef(-3f, 0.0f, -20f);
-		glRotatef(45f, 0.4f, 1.0f, 0.1f);
-		//glColor3f(0.5f, 0.5f, 1.0f);
-
-		for(int x = 0; x<3; x++){
-			for(int y = 0; y<3;y++){
-				for(int z = 0; z<3;z++){
-					renderCube();
-					glTranslatef(0f, 0.0f, 2f);
-				}
-				glTranslatef(0f, 2f, -6f);
-			}
-			glTranslatef(2f, -6f, 0);
+		for (int x = 0; x < 5; x++) {
+			renderCube();
+			GL11.glTranslatef(5, 0, 0);
 		}
 	}
 
 	public static void renderCube() {
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
 
-		Texture tex = MaterialManager.getMaterial("block_stone").getTexture();
+		GL11.glTranslatef(0.0f,0.0f,-5.0f);                              // Move Into The Screen 5 Units
 
-		tex.bind();
-
-		glBegin(GL_QUADS);
-
+		GL11.glBegin(GL11.GL_QUADS);
+		//HERE IS WHERE YOU BIND A TEXTURE
 		// Front Face
-		glTexCoord2f(0.0f, 0.0f);
-		glVertex3f( - 1.0f,  - 1.0f,  + 1.0f); // Bottom Left Of The Texture and Quad
-		glTexCoord2f(1.0f, 0.0f);
-		glVertex3f( + 1.0f,  - 1.0f,  + 1.0f); // Bottom Right Of The Texture and Quad
-		glTexCoord2f(1.0f, 1.0f);
-		glVertex3f( + 1.0f,  + 1.0f,  + 1.0f); // Top Right Of The Texture and Quad
-		glTexCoord2f(0.0f, 1.0f);
-		glVertex3f( - 1.0f,  + 1.0f,  + 1.0f); // Top Left Of The Texture and Quad
+		GL11.glTexCoord2f(0.0f, 0.0f);
+		GL11.glVertex3f(-1.0f, -1.0f,  1.0f);   // Bottom Left Of The Texture and Quad
+		GL11.glTexCoord2f(1.0f, 0.0f);
+		GL11.glVertex3f( 1.0f, -1.0f,  1.0f);   // Bottom Right Of The Texture and Quad
+		GL11.glTexCoord2f(1.0f, 1.0f);
+		GL11.glVertex3f( 1.0f,  1.0f,  1.0f);   // Top Right Of The Texture and Quad
+		GL11.glTexCoord2f(0.0f, 1.0f);
+		GL11.glVertex3f(-1.0f,  1.0f,  1.0f);   // Top Left Of The Texture and Quad
 
 		// Back Face
-		glTexCoord2f(1.0f, 0.0f);
-		glVertex3f( - 1.0f,  - 1.0f,  - 1.0f); // Bottom Right Of The Texture and Quad
-		glTexCoord2f(1.0f, 1.0f);
-		glVertex3f( - 1.0f,  + 1.0f,  - 1.0f); // Top Right Of The Texture and Quad
-		glTexCoord2f(0.0f, 1.0f);
-		glVertex3f( + 1.0f,  + 1.0f,  - 1.0f); // Top Left Of The Texture and Quad
-		glTexCoord2f(0.0f, 0.0f);
-		glVertex3f( + 1.0f,  - 1.0f,  - 1.0f); // Bottom Left Of The Texture and Quad
+		GL11.glTexCoord2f(1.0f, 0.0f);
+		GL11.glVertex3f(-1.0f, -1.0f, -1.0f);   // Bottom Right Of The Texture and Quad
+		GL11.glTexCoord2f(1.0f, 1.0f);
+		GL11.glVertex3f(-1.0f,  1.0f, -1.0f);   // Top Right Of The Texture and Quad
+		GL11.glTexCoord2f(0.0f, 1.0f);
+		GL11.glVertex3f( 1.0f,  1.0f, -1.0f);   // Top Left Of The Texture and Quad
+		GL11.glTexCoord2f(0.0f, 0.0f);
+		GL11.glVertex3f( 1.0f, -1.0f, -1.0f);   // Bottom Left Of The Texture and Quad
 
 		// Top Face
-		glTexCoord2f(0.0f, 1.0f);
-		glVertex3f( - 1.0f,  + 1.0f,  - 1.0f); // Top Left Of The Texture and Quad
-		glTexCoord2f(0.0f, 0.0f);
-		glVertex3f( - 1.0f,  + 1.0f,  + 1.0f); // Bottom Left Of The Texture and Quad
-		glTexCoord2f(1.0f, 0.0f);
-		glVertex3f( + 1.0f,  + 1.0f,  + 1.0f); // Bottom Right Of The Texture and Quad
-		glTexCoord2f(1.0f, 1.0f);
-		glVertex3f(  + 1.0f,  + 1.0f,  - 1.0f); // Top Right Of The Texture and Quad
+		GL11.glTexCoord2f(0.0f, 1.0f);
+		GL11.glVertex3f(-1.0f,  1.0f, -1.0f);   // Top Left Of The Texture and Quad
+		GL11.glTexCoord2f(0.0f, 0.0f);
+		GL11.glVertex3f(-1.0f,  1.0f,  1.0f);   // Bottom Left Of The Texture and Quad
+		GL11.glTexCoord2f(1.0f, 0.0f);
+		GL11.glVertex3f( 1.0f,  1.0f,  1.0f);   // Bottom Right Of The Texture and Quad
+		GL11.glTexCoord2f(1.0f, 1.0f);
+		GL11.glVertex3f( 1.0f,  1.0f, -1.0f);   // Top Right Of The Texture and Quad
 
 		// Bottom Face
-		glTexCoord2f(1.0f, 1.0f);
-		glVertex3f( - 1.0f,  - 1.0f,  - 1.0f); // Top Right Of The Texture and Quad
-		glTexCoord2f(0.0f, 1.0f);
-		glVertex3f( + 1.0f,  - 1.0f,  - 1.0f); // Top Left Of The Texture and Quad
-		glTexCoord2f(0.0f, 0.0f);
-		glVertex3f( + 1.0f,  - 1.0f,  + 1.0f); // Bottom Left Of The Texture and Quad
-		glTexCoord2f(1.0f, 0.0f);
-		glVertex3f( - 1.0f,  - 1.0f,  + 1.0f); // Bottom Right Of The Texture and Quad
+		GL11.glTexCoord2f(1.0f, 1.0f);
+		GL11.glVertex3f(-1.0f, -1.0f, -1.0f);   // Top Right Of The Texture and Quad
+		GL11.glTexCoord2f(0.0f, 1.0f);
+		GL11.glVertex3f( 1.0f, -1.0f, -1.0f);   // Top Left Of The Texture and Quad
+		GL11.glTexCoord2f(0.0f, 0.0f);
+		GL11.glVertex3f( 1.0f, -1.0f,  1.0f);   // Bottom Left Of The Texture and Quad
+		GL11.glTexCoord2f(1.0f, 0.0f);
+		GL11.glVertex3f(-1.0f, -1.0f,  1.0f);   // Bottom Right Of The Texture and Quad
 
 		// Right face
-		glTexCoord2f(1.0f, 0.0f);
-		glVertex3f( + 1.0f,  - 1.0f,  - 1.0f); // Bottom Right Of The Texture and Quad
-		glTexCoord2f(1.0f, 1.0f);
-		glVertex3f( + 1.0f,  + 1.0f,  - 1.0f); // Top Right Of The Texture and Quad
-		glTexCoord2f(0.0f, 1.0f);
-		glVertex3f( + 1.0f,  + 1.0f,  + 1.0f); // Top Left Of The Texture and Quad
-		glTexCoord2f(0.0f, 0.0f);
-		glVertex3f( + 1.0f,  - 1.0f,  + 1.0f); // Bottom Left Of The Texture and Quad
+		GL11.glTexCoord2f(1.0f, 0.0f);
+		GL11.glVertex3f( 1.0f, -1.0f, -1.0f);   // Bottom Right Of The Texture and Quad
+		GL11.glTexCoord2f(1.0f, 1.0f);
+		GL11.glVertex3f( 1.0f,  1.0f, -1.0f);   // Top Right Of The Texture and Quad
+		GL11.glTexCoord2f(0.0f, 1.0f);
+		GL11.glVertex3f( 1.0f,  1.0f,  1.0f);   // Top Left Of The Texture and Quad
+		GL11.glTexCoord2f(0.0f, 0.0f);
+		GL11.glVertex3f( 1.0f, -1.0f,  1.0f);   // Bottom Left Of The Texture and Quad
 
 		// Left Face
-		glTexCoord2f(0.0f, 0.0f);
-		glVertex3f( - 1.0f,  - 1.0f,  - 1.0f); // Bottom Left Of The Texture and Quad
-		glTexCoord2f(1.0f, 0.0f);
-		glVertex3f( - 1.0f,  - 1.0f,  + 1.0f); // Bottom Right Of The Texture and Quad
-		glTexCoord2f(1.0f, 1.0f);
-		glVertex3f( - 1.0f,  + 1.0f,  + 1.0f); // Top Right Of The Texture and Quad
-		glTexCoord2f(0.0f, 1.0f);
-		glVertex3f( - 1.0f,  + 1.0f,  - 1.0f); // Top Left Of The Texture and Quad
-		glEnd();
+		GL11.glTexCoord2f(0.0f, 0.0f);
+		GL11.glVertex3f(-1.0f, -1.0f, -1.0f);   // Bottom Left Of The Texture and Quad
+		GL11.glTexCoord2f(1.0f, 0.0f);
+		GL11.glVertex3f(-1.0f, -1.0f,  1.0f);   // Bottom Right Of The Texture and Quad
+		GL11.glTexCoord2f(1.0f, 1.0f);
+		GL11.glVertex3f(-1.0f,  1.0f,  1.0f);   // Top Right Of The Texture and Quad
+		GL11.glTexCoord2f(0.0f, 1.0f);
+		GL11.glVertex3f(-1.0f,  1.0f, -1.0f);   // Top Left Of The Texture and Quad
+		GL11.glEnd();
 	}
 
 
