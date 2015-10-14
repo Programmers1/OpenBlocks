1b5bbbf801abe3ec4f413574d363454bff484ca9
Mooncoder <mooncricket997@gmail.com>
2015-10-14 12:55:09 -0500

    Update MetadataStore.java

--- src/tk/mattrick/openblocks/entity/MetadataStore.java
+++ src/tk/mattrick/openblocks/entity/MetadataStore.java
@@ -4,5 +4,16 @@ package tk.mattrick.openblocks.entity;
     
     public interface MetadataStore {
     
+       public void setMetadata (T subject, String metadataKey, MetadataValue newMetadataValue);
        
-    }
+       public void removeMetadata(T subject, String metadataKey);
+       
+       public void setMetadata(T subject, String metadataKey, MetadataValue newMetadataValue); 
+ 
+       public List<MetadataValue> getMetadata(T subject, String metadataKey); 
+
+        public boolean hasMetadata(T subject, String metadataKey); 
+
+       
+       
+ }
